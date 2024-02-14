a398d670ef08d4073be829c502aa983e90f8ca8a
imlnr <imlnr2002@gmail.com>
2024-02-14 12:25:19 +0530

    final commit"

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const { connection } = require('./config/db');
 const { userRouter } = require('./routes/user.route');
 const { noteRouter } = require('./routes/note.route');
 require('dotenv').config();
+const cors = require('cors');
+
+
+
 const app = express();
 
+
+app.use(cors());
 app.use(express.json())
 app.use('/users', userRouter);
 app.use('/notes',noteRouter);
