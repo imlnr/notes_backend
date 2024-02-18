636763a4d782008d7133f0145349300ff5cb6182
imlnr <imlnr2002@gmail.com>
2024-02-19 01:57:51 +0530

    final commit

--- routes/note.routes.js
+++ routes/note.routes.js
@@ -1,8 +1,8 @@
-const express=require("express")
-const {NoteModel}=require("../model/note.model")
-const {auth}=require("../middlewares/auth.middleware")
+const express = require("express")
+const { NoteModel } = require("../model/note.model")
+const { auth } = require("../middlewares/auth.middleware")
 
-const noteRouter=express.Router();
+const noteRouter = express.Router();
 
 /**
  * @swagger
@@ -25,75 +25,76 @@ const noteRouter=express.Router();
 
 /**
  * @swagger
- *  notes:
+ * /notes:
  *   post:
  *     summary: Add a new note
- *     description: Creates a new note.
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/note'
+ *     tags: [Users]
  *     responses:
  *       200:
- *         description: Successfully added a new note
+ *         description: Creates a new note.
+ *         requestBody:
+ *           required: true
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/note'
  *       500:
  *         description: Internal server error
  */
-noteRouter.post("/",auth,async(req,res)=>{
-    try{
-        const note=new NoteModel(req.body)
+
+noteRouter.post("/", auth, async (req, res) => {
+    try {
+        const note = new NoteModel(req.body)
         await note.save()
-        res.send({"msg":"New note has been added"}) 
-    } catch(err){
-        res.send({"error":err})
+        res.send({ "msg": "New note has been added" })
+    } catch (err) {
+        res.send({ "error": err })
     }
 })
 
 //get the notes opd logged in user or a user can read his/her notes only
-noteRouter.get("/",auth,async(req,res)=>{
-    try{
+noteRouter.get("/", auth, async (req, res) => {
+    try {
         //userid in notes===userid who is making the request
-        const notes=await NoteModel.find({userID:req.body.userID})
-        res.send({notes})
-    } catch(err){   
-        res.send({"error":err})
+        const notes = await NoteModel.find({ userID: req.body.userID })
+        res.send({ notes })
+    } catch (err) {
+        res.send({ "error": err })
     }
 })
 
-noteRouter.patch("/:noteID",auth,async(req,res)=>{
-    const {noteID}=req.params
-    try{
+noteRouter.patch("/:noteID", auth, async (req, res) => {
+    const { noteID } = req.params
+    try {
         //userID presnt in note === userID in the req.body
-        const note=await NoteModel.findOne({_id:noteID})
-        if(note.userID==req.body.userID){
-            await NoteModel.findByIdAndUpdate({_id:noteID},req.body)
-            res.send({"msg":`The note with ID:${noteID} has been updated`})
+        const note = await NoteModel.findOne({ _id: noteID })
+        if (note.userID == req.body.userID) {
+            await NoteModel.findByIdAndUpdate({ _id: noteID }, req.body)
+            res.send({ "msg": `The note with ID:${noteID} has been updated` })
         } else {
-            res.send({"msg":"you are not authorised"})
+            res.send({ "msg": "you are not authorised" })
         }
-    } catch(err){
-        res.send({"error":err})
+    } catch (err) {
+        res.send({ "error": err })
     }
 })
 
-noteRouter.delete("/:noteID",auth,async(req,res)=>{
-    const {noteID}=req.params
-    try{
+noteRouter.delete("/:noteID", auth, async (req, res) => {
+    const { noteID } = req.params
+    try {
         //userID presnt in note === userID in the req.body
-        const note=await NoteModel.findOne({_id:noteID})
-        if(note.userID==req.body.userID){
-            await NoteModel.findByIdAndDelete({_id:noteID})
-            res.send({"msg":`The note with ID:${noteID} has been deleted`})
+        const note = await NoteModel.findOne({ _id: noteID })
+        if (note.userID == req.body.userID) {
+            await NoteModel.findByIdAndDelete({ _id: noteID })
+            res.send({ "msg": `The note with ID:${noteID} has been deleted` })
         } else {
-            res.send({"msg":"you are not authorised"})
+            res.send({ "msg": "you are not authorised" })
         }
-    } catch(err){
-        res.send({"error":err})
+    } catch (err) {
+        res.send({ "error": err })
     }
 })
 
-module.exports={
+module.exports = {
     noteRouter
 }
