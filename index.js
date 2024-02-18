fdce54d8f3b4080bda13ed15326f61b447f2a2bd
imlnr <imlnr2002@gmail.com>
2024-02-18 23:57:14 +0530

    Final commit

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
-const express=require("express")
-const {connection}=require("./config/db")
-const {userRouter}=require("./routes/user.routes")
-const {noteRouter}=require("./routes/note.routes")
-const cors=require("cors")
+const express = require("express")
+const { connection } = require("./config/db")
+const { userRouter } = require("./routes/user.routes")
+const { noteRouter } = require("./routes/note.routes")
+const cors = require("cors")
 require("dotenv").config()
+const swaggerJsdoc = require('swagger-jsdoc');
 
-const app=express()
+const app = express()
 app.use(express.json())
 app.use(cors())
 // error
-app.use("/users",userRouter)
-app.use("/notes",noteRouter)
 
-app.get('/',(req,res)=>{
-    res.send({"msg":"This is the home page....."})
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'User Management System',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./routes/*.js'],
+};
+
+const openapiSpecification = swaggerJsdoc(options);
+
+app.use("/users", userRouter)
+app.use("/notes", noteRouter)
+
+app.get('/', (req, res) => {
+    res.send({ "msg": "This is the home page....." })
 })
 // app.post('/',(req,res)=>{
 //     const data = req.body;
 //     res.send({"msg":data})
 // })
 
-app.listen(process.env.port,async()=>{
-    try{
+app.listen(process.env.port, async () => {
+    try {
         await connection
         console.log("Connected to the DB")
         console.log(`Server is running at port ${'http://localhost:4500/'}`)
-    } catch(err){
+    } catch (err) {
         console.log(err)
     }
 })
